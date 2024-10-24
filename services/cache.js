2f2dbeb7b1c70c7d9e46d88ed99bfb5a3bdb93b9
notayogilee <notayogilee@gmail.com>
2024-10-24 14:33:59 -0700

    Refactor caching to have user id as key with nested hash. Clear hash with id when new data is added - posted to DB.

--- services/cache.js
+++ services/cache.js
@@ -4,16 +4,17 @@ const util = require("util");
 
 const redisUrl = "redis://127.0.0.1:6379";
 const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function () {
+mongoose.Query.prototype.cache = function (options = {}) {
   this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || "");
+
   return this;
 };
 
 mongoose.Query.prototype.exec = async function () {
-  console.log(this.useCache);
   if (!this.useCache) {
     return exec.apply(this, arguments);
   }
@@ -24,7 +25,7 @@ mongoose.Query.prototype.exec = async function () {
   );
 
   // See if we have a value for 'key' in redis
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.hashKey, key);
 
   // If we do - return that
   if (cacheValue) {
@@ -38,7 +39,15 @@ mongoose.Query.prototype.exec = async function () {
   // If not - issue the query and store the result in redis
   const result = await exec.apply(this, arguments);
 
-  client.set(key, JSON.stringify(result));
+  //   client.hset(this.hashKey, JSON.stringify(result), "EX", 10);
+  client.hset(this.hashKey, key, JSON.stringify(result));
+  client.expire(this.hashKey, 10);
 
   return result;
+};
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey));
+  },
 };
