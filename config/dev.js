344bbe4376e338197acb9058d43670de1ab36c6c
notayogilee <notayogilee@gmail.com>
2024-10-24 10:38:14 -0700

    remove comment out code

--- config/dev.js
+++ config/dev.js
@@ -1,14 +1,3 @@
-// module.exports = {
-//   googleClientID:
-//     "70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com",
-//   googleClientSecret: "8mkniDQOqacXtlRD3gA4n2az",
-//   mongoURI:
-//     "mongodb+srv://notayogilee:ovSZNANZKgH6Bjs0@cluster0.e3h9q.mongodb.net/advnode?retryWrites=true&w=majority&appName=Cluster0",
-//   cookieKey: "123123123",
-// };
-
-// console.log(process.env)
-
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
