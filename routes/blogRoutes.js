84c0b46a2292501298d52384fa8561a8c5b737b8
notayogilee <notayogilee@gmail.com>
2024-10-24 10:14:10 -0700

    Initial commit. Bolierplate setup with basic caching example using a local redis server.

---
+++ routes/blogRoutes.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
+
+const Blog = mongoose.model("Blog");
+
+module.exports = (app) => {
+  app.get("/api/blogs/:id", requireLogin, async (req, res) => {
+    const blog = await Blog.findOne({
+      _user: req.user.id,
+      _id: req.params.id,
+    });
+
+    res.send(blog);
+  });
+
+  app.get("/api/blogs", requireLogin, async (req, res) => {
+    const redis = require("redis");
+    const redisUrl = "redis://127.0.0.1:6379";
+    const client = redis.createClient(redisUrl);
+
+    // work around for useless callbacks
+    const util = require("util");
+    client.get = util.promisify(client.get);
+    const userId = req.user.id;
+
+    // do we have any cached data related to this query
+    const cachedBlogs = await client.get(userId);
+
+    // if yes, then respond to the request right away and return
+    if (cachedBlogs) {
+      console.log("SERVING FROM CACHE");
+      return res.send(JSON.parse(cachedBlogs));
+    }
+
+    // if no, we need to respond to request and update our cache to store the data
+    const blogs = await Blog.find({ _user: userId });
+
+    console.log("SERVING FROM DB");
+    res.send(blogs);
+
+    client.set(userId, JSON.stringify(blogs));
+  });
+
+  app.post("/api/blogs", requireLogin, async (req, res) => {
+    const { title, content } = req.body;
+
+    const blog = new Blog({
+      title,
+      content,
+      _user: req.user.id,
+    });
+
+    try {
+      await blog.save();
+      res.send(blog);
+    } catch (err) {
+      res.send(400, err);
+    }
+  });
+};
