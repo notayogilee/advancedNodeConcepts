26133a429d04a572a525443693db1f7ccee1eb7f
notayogilee <notayogilee@gmail.com>
2024-10-27 12:26:53 -0700

    Set up CI for repo.

--- config/dev.js
+++ config/dev.js
@@ -3,4 +3,5 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
+  redisUrl: "redis://127.0.0.1:6379",
 };
